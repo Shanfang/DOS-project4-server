6a8afc49c6a45f0b8942eba399ef83bbff8a68d9
Shanfang <shanfangzhao@gmail.com>
2017-11-18 16:56:25 -0500

    send_tweet(plain, #, @ all workable) and query tweet are tested

--- lib/server.ex
+++ lib/server.ex
@@ -6,7 +6,7 @@ defmodule Server do
     #    GenServer.start_link(__MODULE__, %{}, [name: :coordinator])
     #end
     def start_link(opts \\ []) do
-        GenServer.start_link(__MODULE__, %{}, opts ++ [name: SERVER])        
+        GenServer.start_link(__MODULE__, :ok, opts ++ [name: SERVER])        
     end
 
     # userID is a string
@@ -15,7 +15,7 @@ defmodule Server do
     end
 
     def send_tweet(tweet, userID) do
-        GenServer.cast(@name, {:process_tweet, tweet, userID})
+        GenServer.cast(@name, {:send_tweet, tweet, userID})
     end
 
     def subscribe(to_subscribe_ID, userID) do
@@ -37,17 +37,17 @@ defmodule Server do
     ######################### callbacks ####################
     
     # init the server with an empty table to store clients
-    def init(%{}) do
-        state = %{hash_tag_table: %{}, mention_table: %{}}
+    def init(:ok) do
+        #state = %{hash_tag_table: %{}, mention_table: %{}}
         #user_table = :ets.new(:user_table, [:named_table, protected: true, read_concurrency: true])
         #hash_tag_table = :ets.new(:hash_tag_table,[:named_table, protected: true, read_concurrency: true])
         #mention_table = :ets.new(:mention_table,[:named_table, protected: true, read_concurrency: true])
         :ets.new(:user_table, [:set, :named_table, :public])
-        :ets.new(:hash_tag_table,[:set, :named_table, :public])
-        :ets.new(:mention_table,[:set, :named_table, :public])
+        :ets.new(:hash_tag_table,[:bag, :named_table, :public])
+        :ets.new(:mention_table,[:bag, :named_table, :public])
         #state = %{"usertable" => :user_table, "hash_tag_table" => :hash_tag_table, "mention_table" => :mention_table}
         #state = %{"usertable" => :user_table, "hash_tag_table" => %{}, "mention_table" => %{}} 
-        {:ok, state}
+        {:ok, :ok}
         #{:ok, %{state |"usertable" => user_table, "hash_tag_table" => hash_tag_table, "mention_table" => mention_table}}
     end
 
@@ -58,7 +58,7 @@ defmodule Server do
         IO.puts "Current user_table is " 
         IO.inspect :ets.lookup(:user_table, userID)
         
-        case check_user_status(userID) do
+        case user_status(userID) do
             :ok ->
                 IO.puts "This user is already registered, please try another one."
             :error ->
@@ -72,7 +72,7 @@ defmodule Server do
     @doc """
     Assuming the user is registered, so there is no need to check if the user exists in user_table
     """
-    def handle_cast({:process_tweet, tweet, userID}, state) do        
+    def handle_cast({:send_tweet, tweet, userID}, state) do        
         user_tuple = :ets.lookup(:user_table, userID) |> List.first
         :ets.insert(:user_table, {userID, elem(user_tuple, 1), elem(user_tuple, 2), [tweet | elem(user_tuple, 3)]})
         
@@ -104,7 +104,7 @@ defmodule Server do
     """
     def handle_call({:subscribe, to_subscribe_ID, userID}, _from, state) do
         user_tuple = :ets.lookup(:user_table, userID) |> List.first
-        case check_user_status(to_subscribe_ID) do
+        case user_status(to_subscribe_ID) do
             :ok ->
                 following_tuple = :ets.lookup(:user_table, to_subscribe_ID) |> List.first
                 :ets.insert(:user_table, {userID, elem(user_tuple, 1), [to_subscribe_ID | elem(user_tuple, 2)], elem(user_tuple, 3)})
@@ -121,9 +121,24 @@ defmodule Server do
         {:reply, userID, state}
     end
     
+    def handle_call({:query_tweet, query, userID}, _from, state) do
+        user_tuple = :ets.lookup(:user_table, userID) |> List.first
+        result =
+            case query_type(query) do
+                :subscribtion ->
+                    elem(user_tuple, 3)
+                {:hash_tag, hash_tag} ->
+                    list1 = :ets.lookup(:hash_tag_table, hash_tag)
+                    form_list(list1, [], length(list1))  
+                {:mention, mention} ->
+                    list2 = :ets.lookup(:mention_table, mention)
+                    form_list(list2, [], length(list2))       
+            end
+        {:reply, result, state}
+    end
     ######################### helper functions ####################
 
-    defp check_user_status(userID) do
+    defp user_status(userID) do
         case :ets.lookup(:user_table, userID) do              
             [{^userID, _, _, _}] -> 
                 :ok
@@ -158,19 +173,21 @@ defmodule Server do
         :ok
     end
 
+    # this will match tweet of hashtag or mention type, with # comes first
+    # so if a tweet is "this is a test #@hi tweet", it is matched as hashtag tweet
     defp tweet_type(tweet) do
         result = 
             cond do
                 tweet =~ "#" ->
                     hash_tag = 
-                    String.split(tweet, ~r{#}, parts: 2)
+                    String.split(tweet, ~r{#}, parts: 2) # regular expression to match # and split with that
                     |> List.last
                     |> String.split(~r{\s}, trim: true)
                     |> List.first
                     {:hash_tag, hash_tag}
                 tweet =~ "@" ->
                     mention = 
-                    String.split(tweet, ~r{@}, parts: 2)
+                    String.split(tweet, ~r{@}, parts: 2) # regular expression to match @ and split with that
                     |> List.last
                     |> String.split(~r{\s}, trim: true)
                     |> List.first
@@ -178,6 +195,46 @@ defmodule Server do
                 true ->
                     :plain_tweet
             end
+        #result
+    end
+
+
+    defp query_type(query) do
+        result = 
+            cond do
+                String.contains?(query, "#") ->
+                    {:hash_tag, String.slice(query, 1..(String.length(query) - 1))}
+                String.contains?(query, "@") ->
+                    {:mention, String.slice(query, 1..(String.length(query) - 1))}
+                true ->
+                    :subscribtion
+            end  
+    end
+
+    @doc """
+    defp query_type(query) do
+        result = [:subscribtion]
+        if String.contains?(query, "#") do
+            result = [:hash_tag | result]
+        end
+        if String.contains?(query, "#") do
+            result = [:mention | result]            
+        end
         result
+    end
+    """
+
+    # change [{"h", "hi"}, {"h", "hello"}, {"h", "how"}] to ["hi", "hello", "how"]
+    defp form_list(tuple_list, result_list, count)  when count > 0 do
+        tweet = tuple_list 
+                |>List.first 
+                |> elem(1)
+        result_list = [tweet | result_list]
+        tuple_list = List.delete_at(tuple_list, 0)
+        form_list(tuple_list, result_list, count - 1)
+    end
+
+    defp form_list(tuple_list, result_list, count) do
+        result_list
     end
  end
